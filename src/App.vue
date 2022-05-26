8fa5552b9c91789d7bea0bce5f7800c3cf880ca2
Thiago099 <thiago.kaique01@gmail.com>
2022-05-26 16:46:36 -0300

    [fix] size

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,29 @@ import lineChart from '@/charts/line-chart.vue'
 </script>
 
 <template>
-  <div class="chart-container">
-    <bar-chart />
-  </div>
-  <div class="chart-container">
-    <doughnut-chart />
-  </div>
-  <div class="chart-container">
-    <line-chart />
+  <div class="container">
+    <div class="chart-container">
+      <bar-chart />
+    </div>
+    <div class="chart-container" style="width:500px">
+      <doughnut-chart />
+    </div>
+    <div class="chart-container">
+      <line-chart />
+    </div>
   </div>
 </template>
 
 <style scoped>
-.chart-container {
+.container {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: space-around;
   align-items: center;
   width: 100%;
-  margin-top: 50px;
-  margin-bottom: 50px;
+}
+.chart-container {
+  width: 800px;
+  margin:50px
 }
 </style>
