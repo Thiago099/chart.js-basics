213128bdf4a29a1920d121aa4656ad03e34eb851
Thiago099 <thiago.kaique01@gmail.com>
2022-05-27 08:08:12 -0300

    [feature] chart type

--- src/charts/bar-chart.vue
+++ src/charts/bar-chart.vue
@@ -56,6 +56,7 @@ onMounted(
     );
     myChart.data.datasets.push({
         label: 'My Second Dataset',
+        type: 'line',
         data: [12, 19, 3, 5, 2, 3],
         backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
